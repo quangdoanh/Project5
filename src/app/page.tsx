6cd12ef30f1360d29fdfbd12b2e52ef7ba2283b8
quangdoanh04072004 <quangdoanh04072004@gmail.com>
2025-03-04 11:30:12 +0700

    Cập nhật lần số 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+"use client"
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @next/next/no-img-element */
 
+import { useEffect, useState } from "react";
 import { onValue, ref } from "firebase/database";
 import { CardItem } from "./components/Card/CardItem";
 import { SongItem } from "./components/Song/SongItem";
@@ -8,22 +10,23 @@ import { Title } from "./components/TitleHeader/Title";
 import { firebaseData } from "./fireBaseConfig";
 
 export default function Home() {
+  const [data, setData] = useState<any[]>([]);
+  const [data2, setData2] = useState<any[]>([]);
+  const [data3, setData3] = useState<any[]>([]);
 
-  // Section 1
-  const data: any[] = [];
-  const SongRef = ref(firebaseData, 'songs');
-  onValue(SongRef, (items) => {
-    items.forEach((item) => {
-      const key = item.key;
-      const datafb = item.val();
+  // Section 1: Fetch songs data
+  useEffect(() => {
+    const SongRef = ref(firebaseData, 'songs');
+    const unsubscribe = onValue(SongRef, (items) => {
+      const newData: any[] = [];
+      items.forEach((item) => {
+        const key = item.key;
+        const datafb = item.val();
 
-
-      onValue(ref(firebaseData, '/singers/' + datafb.singerId[0]), (singer) => {
-
-        const dataSing = singer.val();
-        if (data.length < 3) {
-          data.push(
-            {
+        onValue(ref(firebaseData, '/singers/' + datafb.singerId[0]), (singer) => {
+          const dataSing = singer.val();
+          if (newData.length < 3) {
+            newData.push({
               id: key,
               img: datafb.image,
               namesong: datafb.title,
@@ -32,71 +35,66 @@ export default function Home() {
               link: `/song/${key}`,
               audio: datafb.audio,
               wishlist: datafb.wishlist
-            }
-          )
-        }
+            });
+          }
+        });
       });
-    })
-  });
-  console.log("Trang chủ có data như sau: ")
-  console.log(data)
-  // End Section 
+      setData(newData);
+    });
 
+    return () => unsubscribe();
+  }, []);
 
-  // Section 2
-  const data2: any[] = [];
-  const CategoriesRef = ref(firebaseData, 'categories');
-  onValue(CategoriesRef, (items) => {
-    items.forEach((item) => {
-      const key = item.key;
-      const datafb = item.val();
+  // Section 2: Fetch categories data
+  useEffect(() => {
+    const CategoriesRef = ref(firebaseData, 'categories');
+    const unsubscribe = onValue(CategoriesRef, (items) => {
+      const newData: any[] = [];
+      items.forEach((item) => {
+        const key = item.key;
+        const datafb = item.val();
 
-      console.log(datafb)
-
-      if (data2.length < 5) {
-        data2.push(
-          {
+        if (newData.length < 5) {
+          newData.push({
             id: key,
             img: datafb.image,
             title: datafb.title,
             desc: datafb.description,
             link: `categories/${key}`
-          }
-        )
+          });
+        }
+      });
+      setData2(newData);
+    });
 
-      }
-    })
-  });
+    return () => unsubscribe();
+  }, []);
 
-  // End Section 2
+  // Section 3: Fetch singers data
+  useEffect(() => {
+    const SingeriesRef = ref(firebaseData, 'singers');
+    const unsubscribe = onValue(SingeriesRef, (items) => {
+      const newData: any[] = [];
+      items.forEach((item) => {
+        const key = item.key;
+        const datafb = item.val();
 
-  // Section 3
-
-  const data3: any[] = [];
-  const SingeriesRef = ref(firebaseData, 'singers');
-  onValue(SingeriesRef, (items) => {
-    items.forEach((item) => {
-      const key = item.key;
-      const datafb = item.val();
-
-      console.log(datafb)
-
-      if (data3.length < 5) {
-        data3.push(
-          {
+        if (newData.length < 5) {
+          newData.push({
             id: key,
             img: datafb.image,
             title: datafb.title,
             desc: datafb.description,
             link: `sing/${key}`
-          }
-        )
+          });
+        }
+      });
+      setData3(newData);
+    });
 
-      }
-    })
-  });
+    return () => unsubscribe();
+  }, []);
 
-  // End Section 3
   return (
     <>
       {/* Section 1 */}
@@ -104,12 +102,11 @@ export default function Home() {
         <div className="inner-left w-[60%] flex items-center gap-[33px] pl-[23px] bg-cover rounded-[15px] pt-[40px]"
           style={{ backgroundImage: "url(/bg-1.png)" }}
         >
-          <div className="w-[231px]"
-          >
+          <div className="w-[231px]">
             <h2 className="title text-[32px] font-[700]">Nhạc EDM</h2>
             <p className="desc text-[14px] font-[500]">Top 100 Nhạc Electronic/Dance Âu Mỹ là danh sách 100 ca khúc hot nhất hiện tại của thể loại Top 100 Nhạc Electronic/Dance Âu Mỹ</p>
           </div>
-          <div className="  h-[321px] ">
+          <div className="h-[321px]">
             <img
               src="/avatar.png"
               alt="EDM"
@@ -117,7 +114,7 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="inner-right w-[40%]  ">
+        <div className="inner-right w-[40%]">
           <Title title="Nghe Nhiều" />
           <div className="box grid grid-flow-col-1 gap-y-[10px]">
             {data.map((item, index) => (
@@ -129,7 +126,7 @@ export default function Home() {
       {/* Section 2 */}
       <div className="inner-wrap text-white my-[30px]">
         <Title title="Nhạc Trẻ" />
-        <div className="box grid grid-cols-5 gap-[10px] ">
+        <div className="box grid grid-cols-5 gap-[10px]">
           {data2.map((item, index) => (
             <CardItem key={index} item={item} />
           ))}
@@ -138,15 +135,12 @@ export default function Home() {
       {/* Section 3 */}
       <div className="inner-wrap text-white my-[30px]">
         <Title title="Ca sĩ nổi bật" />
-        <div className="box grid grid-cols-5 gap-[10px] ">
+        <div className="box grid grid-cols-5 gap-[10px]">
           {data3.map((item, index) => (
             <CardItem key={index} item={item} />
           ))}
         </div>
       </div>
-
     </>
-  );
-}
-
-
+  )
+};
